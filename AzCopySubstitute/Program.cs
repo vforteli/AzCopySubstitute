a33ed4c5e389674d7bd91ad7fbd66b684f8c4dd9
Verner Fortelius <verner@fortelius.net>
2021-09-13 22:00:41 +0300

    add support for gracefully handling break
    still needs to actually continue from the right place..

--- AzCopySubstitute/Program.cs
+++ AzCopySubstitute/Program.cs
@@ -17,13 +17,12 @@ namespace AzCopySubstitute
         /// <param name="destinationConnection">Destination connection string with sas token</param>
         /// <param name="recursive"></param>
         /// <param name="threads"></param>
-        /// <param name="overwrite"></param>
+        /// <param name="waitForCopyResult"></param>        
         /// <returns></returns>
-        static async Task Main(string sourceConnection, string destinationConnection, bool recursive = true, bool overwrite = true, int threads = 16)
+        static async Task Main(string sourceConnection, string destinationConnection, bool recursive = true, int threads = 1000, bool waitForCopyResult = false)
         {
             var sourceUri = new Uri(sourceConnection);
             var destinationUri = new Uri(destinationConnection);
-
 
             var sourceDatalakeService = new DataLakeServiceClient(sourceUri);
             var destinationDataLakeService = new BlobServiceClient(destinationUri);
@@ -32,16 +31,36 @@ namespace AzCopySubstitute
             var processedCount = 0;
             var failedCount = 0;
             var totalcount = 0;
+            using var cancellationTokenSource = new CancellationTokenSource();
 
 
             using var semaphore = new SemaphoreSlim(threads, threads);
 
-            var stopwatch = Stopwatch.StartNew();
             var sourceFileTasks = new ConcurrentDictionary<Guid, Task>();
             var copyTasks = new ConcurrentDictionary<Guid, Task>();
+            var currentContinuationToken = "";
+            var currentFileSystem = "";
 
-            await foreach (var filesystem in sourceDatalakeService.GetFileSystemsAsync())
+            Console.CancelKeyPress += (s, e) =>
             {
+                if (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    e.Cancel = true;
+                    Console.WriteLine("Breaking, waiting for queued tasks to complete. Press break again to force stop");
+                    cancellationTokenSource.Cancel();
+                }
+                else
+                {
+                    Console.WriteLine("Terminating threads");
+                    Environment.Exit(1);
+                }
+            };
+
+            var stopwatch = Stopwatch.StartNew();
+            await foreach (var filesystem in sourceDatalakeService.GetFileSystemsAsync(cancellationToken: cancellationTokenSource.Token))
+            {
+                currentFileSystem = filesystem.Name;
+
                 var sourceFileSystemClient = sourceDatalakeService.GetFileSystemClient(filesystem.Name);
                 var destinationFileSystemClient = destinationDataLakeService.GetBlobContainerClient(filesystem.Name);
                 await destinationFileSystemClient.CreateIfNotExistsAsync();
@@ -51,21 +70,34 @@ namespace AzCopySubstitute
                 Console.WriteLine("Starting list files task");
                 var listFilesTask = Task.Run(async () =>
                 {
-                    await foreach (var sourcePath in sourceFileSystemClient.GetPathsAsync(recursive: recursive))
+                    try
                     {
-                        if (!sourcePath.IsDirectory ?? false)
+                        await foreach (var sourcePathPage in sourceFileSystemClient.GetPathsAsync(recursive: recursive, cancellationToken: cancellationTokenSource.Token).AsPages(pageSizeHint: 5000))
                         {
-                            sourcePathNames.Add(sourcePath.Name);
-                            var currentCount = Interlocked.Increment(ref filesCount);
-                            if (currentCount % 10000 == 0)
+                            foreach (var sourcepath in sourcePathPage.Values)
                             {
-                                Console.WriteLine($"Found {currentCount} files...");
+                                sourcePathNames.Add(sourcepath.Name);
+                                var currentCount = Interlocked.Increment(ref filesCount);
+                                if (currentCount % 10000 == 0)
+                                {
+                                    Console.WriteLine($"Found {currentCount} files...");
+                                }
                             }
+
+                            currentContinuationToken = sourcePathPage.ContinuationToken;
                         }
                     }
-
-                    sourcePathNames.CompleteAdding();
-                    Console.WriteLine($"List files done. Found {filesCount} total files");
+                    catch (TaskCanceledException)
+                    {
+                        Console.WriteLine("\n\nStopping list path task. To continue from here use token:");
+                        Console.WriteLine($"ContinuationToken: {currentContinuationToken}");
+                        Console.WriteLine($"FileSystem: {currentFileSystem}\n\n");
+                    }
+                    finally
+                    {
+                        sourcePathNames.CompleteAdding();
+                        Console.WriteLine($"List files done. Found {filesCount} total files to copy");
+                    }
                 });
 
                 Console.WriteLine("Starting consume tasks");
@@ -84,7 +116,10 @@ namespace AzCopySubstitute
                                 var destinationFileClient = destinationFileSystemClient.GetBlobClient(sourcePath);
                                 var status = await destinationFileClient.StartCopyFromUriAsync(sourceFileClient.Uri);
 
-                                copyTasks.TryAdd(taskId, status.WaitForCompletionAsync().AsTask().ContinueWith((o) => { copyTasks.TryRemove(taskId, out _); }));
+                                if (waitForCopyResult)
+                                {
+                                    copyTasks.TryAdd(taskId, status.WaitForCompletionAsync().AsTask().ContinueWith((o) => { copyTasks.TryRemove(taskId, out _); }));
+                                }
 
                                 var currentCount = Interlocked.Increment(ref processedCount);
                                 if (currentCount % 10000 == 0)
@@ -111,7 +146,7 @@ namespace AzCopySubstitute
 
                 Console.WriteLine("Waiting for list files and iterate tasks");
                 await Task.WhenAll(listFilesTask, iterateTask);
-                Console.WriteLine("Waiting for source file task");
+                Console.WriteLine($"Waiting for {sourceFileTasks.Count} source file tasks to complete");
                 await Task.WhenAll(sourceFileTasks.Values);
                 Console.WriteLine($"All copy tasks have been started, waiting for {copyTasks.Count} to complete...");
                 await Task.WhenAll(copyTasks.Values);
